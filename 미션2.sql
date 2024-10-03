fcb1d11787e23fe600503346e5db944d4efdde26
MeongW <easymiracle@skuniv.ac.kr>
2024-10-04 02:00:26 +0900

    week2

--- 미션2.sql
+++ 미션2.sql
@@ -1,8 +1,11 @@
 # 1. 멤버 미션 조회 쿼리(페이징 포함)
-select * from member_mission where created_at <
+select mm.*, s.name as store_name from member_mission as mm
+join mission as m on mm.mission_id = m.id
+join store s on m.store_id = s.id
+where mm.created_at <
     (select created_at from member_mission where id = 3)
-    AND id < 3
-order by created_at desc limit 15;
+    and mm.id < 3
+order by mm.id desc limit 15;
 
 # 2. 리뷰 조회 쿼리
 select m.name, r.body, r.score, r.created_at, r.updated_at
@@ -16,10 +19,16 @@ select point, spec_address from member where id = 1;
 # 미션 성공 개수
 select count(*) from member_mission where member_id = 1 and status like '성공';
 # 지역 미션
-select * from mission where created_at <
-    (select created_at from member_mission where id = 3)
-    AND id < 3
-order by id desc limit 15;
+select m.*, s.name as store_name from mission as m
+join store as s on m.store_id = s.id
+join region as r on s.region_id = r.id
+join member as mem on r.name = mem.spec_address
+where
+    mem.id = 1 and
+    m.deadline > NOW() and
+    m.created_at < (select created_at from member_mission where id = 3) and
+    m.id < 3
+order by m.id desc limit 15;
 
 # 4. 마이 페이지 조회 쿼리
 select * from mission as m
